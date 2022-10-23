7290f130d33ccab5c66ae5664d7370c58e8eb998
jfjeld15 <jfjeld@ualberta.ca>
2022-10-23 13:08:28 -0600

    AAAAAAAAAAAAAAAAAAA

--- c291Mini1.py
+++ c291Mini1.py
@@ -7,7 +7,6 @@ Jonathan Fjeld, Ying Wan, Crystal Zhang
 import sqlite3
 import sys  # For taking command line argument
 from getpass import getpass  # For making the password non-visible at the time of typing
-import hashlib  # Used for hashing passwords to prevent SQL injection
 
 def setup(dbName):
     # connects to an sql db, returns a cursor and conection variables
@@ -26,18 +25,25 @@ def getID():
             print("Please enter a valid ID (between 1-4 characters)") # Loop again
     return id
 
+def newUser(id, pwd, c, conn):
+    name = input("New user detected! Please enter your name to create an account : ").lower
+    c.execute("INSERT INTO users VALUES (?, ?, ?);", (id, name, pwd))
+    conn.commit()
+
+
 def login(id, pwd, c, conn):
     isUser = False
     isArtist = False
-    c.execute('SELECT * FROM users WHERE pwd = ?;', (pwd,))  # This format escapes special characters (prevents SQL injection)
-    print(c.fetchall())
-    return
-    # if len(c.fetchall()) != 0:
-    #     isUser = True
-    # c.execute('SELECT * FROM artists WHERE aid = ?;', id,)
-    # if len(c.fetchall()) != 0:
-    #     isArtist = True
-    # if isUser == False and isArtist == False:
+     # This format escapes special characters (prevents SQL injection):
+    c.execute("SELECT * FROM users WHERE lower(uid) = ? AND pwd = ?;", (id, pwd,)) 
+    if len(c.fetchall()) != 0:
+        isUser = True
+    c.execute("SELECT * FROM artists WHERE lower(aid) = ? AND pwd = ?;", (id, pwd,))
+    if len(c.fetchall()) != 0:
+        isArtist = True
+    if isUser == False and isArtist == False:
+        # User does not exist, create one!
+        newUser(id, pwd, c, conn)
 
 
 if __name__ == "__main__":
